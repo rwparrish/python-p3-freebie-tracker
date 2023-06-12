1b154dc1438a9356f6bbf8a99a2f4c54aa64966c
rwparrish <60946750+rwparrish@users.noreply.github.com>
2023-06-11 19:35:18 -0600

    working seed data

--- lib/seed.py
+++ lib/seed.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import Faker
+from faker import Faker
 from random import choice as rc
 
 from sqlalchemy import create_engine
@@ -21,9 +21,9 @@ def delete_records():
     session.commit()
     
 def create_records():
-    companies = [Company(fake.name(), fake.random_int(min=1930, max=2023)) for i in range(20)]
-    freebies = [Freebie(fake.name(), fake.random_int(min=1, max=117)) for i in range(100)]
-    devs = [Dev(fake.name()) for i in range(50)]
+    companies = [Company(name=fake.name(), founding_year=fake.random_int(min=1930, max=2023)) for i in range(20)]
+    freebies = [Freebie(item_name=fake.name(), value=fake.random_int(min=1, max=117)) for i in range(100)]
+    devs = [Dev(name=fake.name()) for i in range(50)]
     session.add_all(companies + freebies + devs)
     session.commit()
     return companies, freebies, devs
