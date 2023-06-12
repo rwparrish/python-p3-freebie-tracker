758b652026b7bcd4f058e9b048f0676a078a0309
rwparrish <60946750+rwparrish@users.noreply.github.com>
2023-06-11 19:24:22 -0600

    tables and seed data

--- lib/models.py
+++ lib/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.associationproxy import association_proxy
 
 convention = {
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -15,15 +16,47 @@ class Company(Base):
     id = Column(Integer(), primary_key=True)
     name = Column(String())
     founding_year = Column(Integer())
+    
+    freebies = relationship('Freebie', back_populates='company', cascade='all, delete-orphan')
+    
+    # not sure I can explain this:
+    devs = association_proxy('freebies', 'dev',
+        creator=lambda dv: Freebie(dev=dv))
 
     def __repr__(self):
         return f'<Company {self.name}>'
+    
 
 class Dev(Base):
     __tablename__ = 'devs'
 
     id = Column(Integer(), primary_key=True)
-    name= Column(String())
+    name = Column(String())
+    
+    freebies = relationship('Freebie', back_populates='dev', cascade='all, delete-orphan')
+    # not sure I can explain this:
+    companies = association_proxy('freebies', 'company',
+        creator=lambda cpy: Freebie(company=cpy))
 
     def __repr__(self):
         return f'<Dev {self.name}>'
+
+
+class Freebie(Base):
+    __tablename__ = 'freebies'
+    
+    id = Column(Integer(), primary_key=True)
+    item_name = Column(String())
+    value = Column(Integer())
+    
+    company_id = Column(Integer(), ForeignKey('companies.id'))
+    dev_id = Column(Integer(), ForeignKey('devs.id'))
+    
+    company = relationship('Company', back_populates='freebies')
+    dev = relationship('Dev', back_populates='freebies')
+    
+    def __repr__(self):
+        return f'<Freebie {self.name}>'
+    
+    
+    
--- lib/seed.py
+++ lib/seed.py
@@ -1,3 +1,44 @@
 #!/usr/bin/env python3
+import Faker
+from random import choice as rc
 
-# Script goes here!
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from models import Company, Freebie, Dev
+
+engine = create_engine('sqlite:///freebies.db')
+Session = sessionmaker(bind=engine)
+session = Session()
+
+fake = Faker()
+
+
+def delete_records():
+    session.query(Company).delete()
+    session.query(Freebie).delete()
+    session.query(Dev).delete()
+    session.commit()
+    
+def create_records():
+    companies = [Company(fake.name(), fake.random_int(min=1930, max=2023)) for i in range(20)]
+    freebies = [Freebie(fake.name(), fake.random_int(min=1, max=117)) for i in range(100)]
+    devs = [Dev(fake.name()) for i in range(50)]
+    session.add_all(companies + freebies + devs)
+    session.commit()
+    return companies, freebies, devs
+
+def relate_records(companies, freebies, devs):
+    for freebie in freebies:
+        freebie.dev = rc(devs)
+        freebie.company = rc(companies)
+
+    session.add_all(freebies)
+    session.commit()
+    
+if __name__ == '__main__':
+    delete_records()
+    companies, freebies, devs = create_records()
+    relate_records(companies, freebies, devs)
+
+
